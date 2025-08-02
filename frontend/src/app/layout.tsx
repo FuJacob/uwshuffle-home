5352c9b98ae1ab186787412bb2fc1b9f2e3031c3
Jacob Fu <jjacobfu@gmail.com>
2025-08-02 06:00:55 -0400

    this is it so far

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Lexend } from "next/font/google";
+import { Lexend, Hanken_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const lexend = Lexend({
   variable: "--font-lexend",
+  subsets: ["latin"],
+});
+
+const hankenGrotesk = Hanken_Grotesk({
+  variable: "--font-hanken-grotesk",
   subsets: ["latin"],
 });
 
@@ -20,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${lexend.variable} antialiased`}>{children}</body>
+      <body className={`${lexend.variable} ${hankenGrotesk.variable} antialiased`}>{children}</body>
     </html>
   );
 }
