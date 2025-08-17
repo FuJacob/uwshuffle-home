b4b3d23a23ffdfaba115be6d67f10d24d21672a9
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2025-08-17 17:20:31 -0400

    aight bet

--- frontend/src/app/help/page.tsx
+++ frontend/src/app/help/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function HelpPage() {
-  redirect("https://youtube.com");
+  redirect("https://www.youtube.com/watch?v=TtSnpr8bm3E");
 }
