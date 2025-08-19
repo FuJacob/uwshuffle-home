2d496b4810e033722a5d8dafd1b3993bd11437c6
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2025-08-18 21:00:02 -0400

    ad

--- frontend/src/constants/headlines.ts
+++ frontend/src/constants/headlines.ts
@@ -9,11 +9,15 @@ export const headlines: Headline[] = [
     headline: "Then UW Shuffle is your headphones.",
   },
   {
+    subtext: "If Quest is getting ranked on WaterlooWorks",
+    headline: "Then UW Shuffle is your Shopify offer.",
+  },
+  {
     subtext: "If Quest is eating 6 lines of Lazeez.",
     headline: "Then UW Shuffle is the bathrooms at E7.",
   },
   {
-    subtext: "If Quest is mandatory PD coursework.",
+    subtext: "If Quest is mandatory PD 11 report.",
     headline: "Then UW Shuffle is GPT Deep Research.",
   },
   {
@@ -23,10 +27,6 @@ export const headlines: Headline[] = [
   {
     subtext: "If Quest is a goose attacking you outside MC.",
     headline: "Then UW Shuffle is a coyote eating goose.",
-  },
-  {
-    subtext: "If Quest is no offers on WaterlooWorks.",
-    headline: "Then UW Shuffle is the external offer.",
   },
   {
     subtext: "If Quest is waiting in line for morning SLC Timmies.",
